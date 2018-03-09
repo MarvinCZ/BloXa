aec8c35acf1ef9fb6a2caef7c861c5a791babdd8
Martin Bruna <mbrunas.p@gmail.com>
2018-03-10 00:55:25 +0100

    init mod

---
+++ src/main/java/cz/youngdevelopers/bloxa/BloxaMod.java
@@ -0,0 +1,31 @@
+package cz.youngdevelopers.bloxa;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = BloxaMod.MODID, name = BloxaMod.NAME, version = BloxaMod.VERSION)
+public class BloxaMod
+{
+    public static final String MODID = "bloxa";
+    public static final String NAME = "BloXa";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
